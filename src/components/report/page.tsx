0ffc8a58199d85b418c96a8f83037af5ce7e96e5
0xaabb12 <0xaabb12@gmail.com>
2025-09-07 17:16:26 +0800

    feat: update

---
+++ src/components/report/page.tsx
@@ -0,0 +1,49 @@
+import { useMemo } from "react";
+import { Globe } from 'lucide-react';
+
+type Page = {
+    type: string,
+    title: string,
+    url: string,
+}
+
+interface PageItemProps {
+    page: Page
+}
+const PageItem = ({page}:PageItemProps) => {
+    return (
+        <div className="px-2 py-1 rounded-20">
+            <a href={page.url} target="_blank" className="flex items-center">
+                <Globe className="h-5 w-5" />
+                <span className="ml-1 font-medium">{page.title}</span>
+            </a>
+        </div>
+    );
+}
+
+interface PageListViewProps {
+    source: string
+}
+const PageListView = ({ source }:PageListViewProps) => {
+
+    const pages = useMemo(() => {
+        if (source) {
+            const params = JSON.parse(source);
+            return params as Page[]
+        }
+        return []
+    }, [source]);
+
+    console.log("pages", pages)
+
+    return (
+        <div className="flex flex-col gap-1 max-h-[200px] overflow-auto mb-1">
+            {pages.map((w, i) => (
+                <PageItem key={i} page={w} />
+            ))}
+        </div>
+    )
+
+}
+
+export default PageListView;
---
+++ src/components/report/website.tsx
@@ -0,0 +1,53 @@
+import { useMemo } from "react";
+import { Globe } from 'lucide-react';
+
+type WebsiteOffice = {
+    id: string,
+    name: string,
+    website: string,
+    logo: string,
+}
+
+interface WebsiteProps {
+    web: WebsiteOffice
+}
+const Website = ({web}:WebsiteProps) => {
+    return (
+        <div className="px-2 py-1 rounded-20">
+            <a href={web.website} target="_blank" className="flex items-center">
+                {web.logo ?
+                    <img className="h-5 w-5 rounded-full" src={web.logo} alt={web.name} /> :
+                    <Globe className="h-5 w-5" />
+                }
+                <span className="ml-1 font-medium">{web.name}</span>
+            </a>
+        </div>
+    );
+}
+
+interface WebsiteListViewProps {
+    source: string
+}
+const WebsiteListView = ({ source }:WebsiteListViewProps) => {
+
+    const websites = useMemo(() => {
+        if (source) {
+            const params = JSON.parse(source);
+            return params as WebsiteOffice[]
+        }
+        return []
+    }, [source]);
+
+    console.log("websites", websites)
+
+    return (
+        <div className="flex flex-col gap-1 max-h-[200px] overflow-auto mb-1">
+            {websites.map((w, i) => (
+                <Website key={i} web={w} />
+            ))}
+        </div>
+    )
+
+}
+
+export default WebsiteListView;
